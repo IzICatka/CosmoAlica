286b86a043401d5f06662abcbbd40fd3b5c1785e
IzICatka <sportandrock@mail.ru>
2023-01-21 01:11:49 +0300

    bot_command

--- bot_telegram.py
+++ bot_telegram.py
@@ -7,6 +7,32 @@ import os
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(bot)
 
+async def on_startup(_):
+    print('Бот вышел в онлайн')
+
+#CLIENT PATH
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message : types.Message):
+    try:
+        await bot.send_message(message.from_user.id, 'Выберите планету, которую вы хотите посетить.')
+        await message.delete()
+    except:
+        await message.reply('Для оформления билетов напишите нашему боту: \nhttp://t.me/CosmoAlicaBot')
+
+@dp.message_handler(commands=['Режим_работы'])
+async def CosmoAlica_open_command(message : types.Message):
+    await bot.send_message(message.from_user.id, 'Вс-Чт с 9:00 до 20:00, Пт-Сб с 10:00 до 23:00')
+
+@dp.message_handler(commands=['Расположение'])
+async def CosmoAlica_place_command(message : types.Message):
+    await bot.send_message(message.from_user.id, 'ул. Научный центр, 5')
+
+
+
+
+
+
+
 @dp.message_handler()
 async def echo_send(message : types.Message):
     if message.text == message.text:
@@ -16,4 +42,4 @@ async def echo_send(message : types.Message):
 
 
 
-executor.start_polling(dp, skip_updates=True)
+executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
